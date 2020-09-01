60128ef543b18458e698f378f92ba3df9178556b
Fred Porciúncula <fred.porciuncula@nufin.de>
2020-09-01 17:27:22 +0200

    Add comment about existing issue

--- app/src/main/java/com/tfcporciuncula/brokentransition/MainActivity.kt
+++ app/src/main/java/com/tfcporciuncula/brokentransition/MainActivity.kt
@@ -10,6 +10,7 @@ class MainActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
     // by uncommenting this two issues are fixed:
     // - the red fragment never blinks in the beginning of the transition from the blue frag to the red frag
+    //   (turns out this issue had already been reported here: https://issuetracker.google.com/issues/163084315)
     // - pressing back during the transition from the blue frag to the red frag won't result in a blank screen
 //    FragmentManager.enableNewStateManager(false)
     setContentView(R.layout.activity_main)
